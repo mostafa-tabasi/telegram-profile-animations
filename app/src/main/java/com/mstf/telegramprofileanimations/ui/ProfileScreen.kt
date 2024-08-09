b85a1240188ab02cd80e535793177196a3d1fac1
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-08-09 16:41:16 +0800

    design: Added profile screen

--- app/src/main/java/com/mstf/telegramprofileanimations/ui/ProfileScreen.kt
+++ app/src/main/java/com/mstf/telegramprofileanimations/ui/ProfileScreen.kt
@@ -1,15 +1,20 @@
 package com.mstf.telegramprofileanimations.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -17,19 +22,106 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceIn
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
 import com.mstf.telegramprofileanimations.R
 
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier) {
+    val minHeaderHeight = remember { 60.dp }
+    val maxHeaderHeight = remember { 240.dp }
+    var currentHeaderHeight by remember { mutableStateOf(minHeaderHeight) }
+    var headerHeightDelta by remember { mutableStateOf(0f) }
+
+    val minAvatarSize = remember { 50.dp }
+    val maxAvatarSize = remember { 100.dp }
+    var currentAvatarSize by remember { mutableStateOf(minAvatarSize) }
+
+    var isHeaderExpanded by remember { mutableStateOf(false) }
+
+    val backButtonSize = remember { 50.dp }
+    val backButtonPadding = remember { 4.dp }
+
+    val items = remember {
+        (1..100).map { "Item #$it" }
+    }
+
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                val delta = available.y.toInt()
+                val newHeaderHeight = currentHeaderHeight + delta.dp.div(
+                    if (!isHeaderExpanded) 5 else 15
+                )
+                val previewHeaderHeight = currentHeaderHeight
+                currentHeaderHeight = newHeaderHeight.coerceIn(minHeaderHeight, maxHeaderHeight)
+                headerHeightDelta =
+                    (currentHeaderHeight - minHeaderHeight) / (maxHeaderHeight - minHeaderHeight)
+                isHeaderExpanded = headerHeightDelta > 0.5
+
+                return Offset(
+                    x = 0f,
+                    y = if (currentHeaderHeight == previewHeaderHeight) 0f else available.y,
+                )
+            }
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .nestedScroll(nestedScrollConnection),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(currentHeaderHeight)
+                .background(Color(0xFF2187B9)),
+        ) {
+            if (!isHeaderExpanded) CollapsedHeaderComposable(
+                backButtonPadding,
+                backButtonSize,
+                headerHeightDelta,
+                minHeaderHeight,
+                currentAvatarSize
+            ) else ExpandedHeaderComposable(
+                currentHeaderHeight,
+                backButtonSize,
+                backButtonPadding
+            )
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            items(items) {
+                Text(
+                    it,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 12.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 private fun BoxScope.CollapsedHeaderComposable(
